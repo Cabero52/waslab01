45f9fc3b7aac27ec4ad044c35f034238044e654c
alejandra <alejandra.gallardo@estudiantat.upc.edu>
2023-02-18 16:30:17 +0100

    Tasca4 a mitges

--- waslab01_cs/src/asw01cs/SimpleFluentClient.java
+++ waslab01_cs/src/asw01cs/SimpleFluentClient.java
@@ -1,6 +1,7 @@
 package asw01cs;
 
 
+import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 //This code uses the Fluent API
 
@@ -11,6 +12,11 @@ public class SimpleFluentClient {
 	public final static void main(String[] args) throws Exception {
     	
     	/* Insert code for Task #4 here */
+    	String author = "Joana";
+    	String tweet_text = "Hola! Soc producte de la tasca 4.";
+    	Request.Post(URI)
+    	.bodyForm(Form.form().add("username",  author).add("tweet_text: ", tweet_text).build()).addHeader("Accept", "text/plain")
+    	.execute().returnContent();
     	
     	System.out.println(Request.Get(URI).addHeader("Accept", "text/plain").execute().returnContent());
     	
